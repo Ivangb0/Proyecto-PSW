7069cdf64ab6de29c16c7d1b506f86ceddd8fd07
Ivangb0 <ivangarciabueno02002@gmail.com>
2023-03-27 20:39:47 +0200

    Cambios en interfaces

---
+++ app/src/main/java/com/example/odswix/Perfil.java
@@ -0,0 +1,15 @@
+package com.example.odswix;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class Perfil extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.perfil);
+
+    }
+}
--- app/src/main/java/com/example/odswix/Registrarse.java
+++ app/src/main/java/com/example/odswix/Registrarse.java
@@ -1,6 +1,5 @@
 package com.example.odswix;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +7,11 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Registrarse extends Activity{
 
@@ -17,7 +21,6 @@ public class Registrarse extends Activity{
 
         //Prueba: paso de datos
         Bundle datos = getIntent().getExtras();
-
         String text = datos.getString("ejemplo");
         TextView texto = findViewById(R.id.contraseña);
         texto.setText(text);
@@ -26,12 +29,35 @@ public class Registrarse extends Activity{
     public void volver(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-
     }
 
     public void registro(View vista){
-        EditText textUsuario= (EditText) findViewById(R.id.usuario);
         EditText textEmail= (EditText) findViewById(R.id.email);
         textEmail.setText("Primera prueba");
+
+        //Prueba de conexión a la base de datos
+        String url = "jdbc:mysql://wixserver.mysql.database.azure.com:3306/wixdatabase?useSSL=true";
+        String user = "KogMaw";
+        String password = "WIXAdmin1";
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT *");
+
+
+            String sql = rs.getString(0);
+            EditText textUsuario= (EditText) findViewById(R.id.usuario);
+            textUsuario.setText("Error");
+            if(sql.isEmpty()){
+                textUsuario.setText("Error");
+            } else {
+                textUsuario.setText(sql);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        EditText textUsuario= (EditText) findViewById(R.id.usuario);
+        textUsuario.setText("Error");
     }
 }

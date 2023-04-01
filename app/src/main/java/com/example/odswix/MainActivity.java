cf292664db5db18a43cf48a3598c7b7f9c11d2f0
Ivangb0 <ivangarciabueno02002@gmail.com>
2023-04-01 16:46:07 +0200

    Avances en registrarse y cosas interesantes

--- app/src/main/java/com/example/odswix/MainActivity.java
+++ app/src/main/java/com/example/odswix/MainActivity.java
@@ -9,21 +9,12 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.sql.Connection;
-
-import BusinessLogic.User;
-import Persistence.UserDAO;
+import java.sql.DriverManager;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
-
-    private static final String DRIVER = "com.mysql.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://wixserver.mysql.database.azure.com:3306/wixdatabase?useSSL=true";
-
-    private static final String USERNAME = "KogMaw";
-    private static final String PASSWORD = "WIXAdmin1";
-    private Connection connection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,31 +25,32 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(threadPolicy);
 
-    }
-    //Metodo de prueba para transferir datos
-    public void ejecutar_registrarse(View view) {
-        Intent intent = new Intent(this, IniciarSesion.class);
+        String url = "jdbc:mysql://wixserver.mysql.database.azure.com:3306/wixdatabase?useSSL=true";
+        String user = "KogMaw";
+        String password = "WIXAdmin1";
 
-        //TextView text = (TextView) findViewById(R.id.texto);
-        //intent.putExtra("ejemplo", text.getText());
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
+        } catch (java.sql.SQLException e) {
+            e.printStackTrace();
+        }
+
+        Intent intent = new Intent(this, Registrarse.class);
 
         startActivity(intent);
     }
 
-    public void buttonConnectToMySQL(View view){
-        try {
-            UserDAO userPrueba = new UserDAO();
-            userPrueba.guardar(new User(15, "paquito","paquito2@gmail.com", "pacoelmejor",100,5,5));
+    //Metodo de prueba para transferir datos    //intent.putExtra("ejemplo", stmt);
 
-        }
-        catch (Exception e){
-            textView.setText(e.toString());
-            System.out.print(e.toString());
-        }
+    public void ejecutar_registrarse(View view) {
+
+    }
+
+    public void buttonConnectToMySQL(View view){
+
     }
 
     public void datosTrans(View vista){
-        TextView texto = (TextView) findViewById(R.id.texto);
-        texto.setText("");
+
     }
 }
--- app/src/main/java/com/example/odswix/Registrarse.java
+++ app/src/main/java/com/example/odswix/Registrarse.java
@@ -7,73 +7,85 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Registrarse extends Activity{
+
+    String url = "jdbc:mysql://wixserver.mysql.database.azure.com:3306/wixdatabase?useSSL=true";
+    String user = "KogMaw";
+    String password = "WIXAdmin1";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registrarusuario);
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-        }catch(ClassNotFoundException e){
-            this.setTitle(".....Class com.mysql.jdbc.Driver not found!");
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            this.setTitle("Illegal access");
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            this.setTitle("instantiation exc eption");
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-         TextView Erruser = findViewById(R.id.Euser);
-         TextView Erremail = findViewById(R.id.Eemail);
-         TextView Errpassword = findViewById(R.id.Epassword);
-
-        Erruser.setVisibility(View.VISIBLE);
-
-        //Prueba: paso de datos
-        Bundle datos = getIntent().getExtras();
-        String text = datos.getString("ejemplo");
-        TextView texto = findViewById(R.id.contraseña);
-        texto.setText(text);
     }
 
-    public void volver(View view){
-        Intent intent = new Intent(this, MainActivity.class);
+    public void iniciarSes(View view){
+        Intent intent = new Intent(this, IniciarSesion.class);
         startActivity(intent);
     }
 
-    public void registro(View vista){
-        EditText textEmail= (EditText) findViewById(R.id.email);
-        textEmail.setText("Primera prueba");
+    public void emptyTest(){
+        TextView Eempty = findViewById(R.id.Eempty);
+        EditText textUser= findViewById(R.id.usuario);
+        TextView textEmail = findViewById(R.id.email);
+        TextView textPass = findViewById(R.id.contraseña);
 
-        //Prueba de conexión a la base de datos
-        String url = "jdbc:mysql://wixserver.mysql.database.azure.com:3306/wixdatabase?useSSL=true";
-        String user = "KogMaw";
-        String password = "WIXAdmin1";
+        if(textUser.getText().toString().isEmpty()){
+            Eempty.setVisibility(View.VISIBLE);
+        } else { Eempty.setVisibility(View.INVISIBLE);}
+
+        if(textEmail.getText().toString().isEmpty()){
+            Eempty.setVisibility(View.VISIBLE);
+        } else { Eempty.setVisibility(View.INVISIBLE);}
+
+        if(textPass.getText().toString().isEmpty()){
+            Eempty.setVisibility(View.VISIBLE);
+        } else { Eempty.setVisibility(View.INVISIBLE);}
+
+    }
+
+    public void userTest(){
+        EditText textUser= findViewById(R.id.usuario);
+        String bduser = textUser.getText().toString();
+        String sql = "";
         try {
             Connection con = DriverManager.getConnection(url, user, password);
-
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT username FROM user");
-            String sql = "";
+
             while (rs.next()) {
                 sql = rs.getString(1);
             }
-            EditText textUsuario= (EditText) findViewById(R.id.usuario);
-
-                textUsuario.setText(sql);
-
-
-
         } catch (java.sql.SQLException e) {
             e.printStackTrace();
         }
+        TextView Erruser = findViewById(R.id.Euser);
+        if(sql.equals(bduser)){
+            Erruser.setVisibility(View.VISIBLE);
+        }
+        else{
+            Erruser.setVisibility(View.INVISIBLE);
+        }
+    }
+    public void registro(View vista){
+        TextView Erruser = findViewById(R.id.Euser);
+        TextView Erremail = findViewById(R.id.Eemail);
+        TextView Errpassword = findViewById(R.id.Epassword);
+        EditText textUser= findViewById(R.id.usuario);
+        TextView textEmail = findViewById(R.id.email);
+        TextView textPass = findViewById(R.id.contraseña);
 
+        emptyTest(); userTest();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Código para evitar que se cierre la actividad al pulsar el botón de Atrás
     }
 }

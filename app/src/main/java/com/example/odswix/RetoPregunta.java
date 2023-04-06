c64a947c3309d27d79bedfa1046bd0100c38bc4f
miguelheru <miguelangelheru@gmail.com>
2023-04-06 21:25:05 +0200

    timer tiempo y popup del timer

--- app/src/main/java/com/example/odswix/RetoPregunta.java
+++ app/src/main/java/com/example/odswix/RetoPregunta.java
@@ -1,10 +1,15 @@
 package com.example.odswix;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.sql.Array;
@@ -31,6 +36,13 @@ public class RetoPregunta extends AppCompatActivity{
     //Lista de respuestas de la pregunta actual pregActual
     List<Answer> respuestasPreg = new ArrayList<Answer>();
     Question pregActual;
+
+    //barra de progreso de tiempo
+    ProgressBar progressBar;
+    Handler handler;
+    Runnable runnable;
+    int progress = 0;
+    int[] colors = {Color.GREEN, Color.GREEN, Color.GREEN, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.RED, Color.RED, Color.RED};
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.retopregunta);
@@ -65,6 +77,41 @@ public class RetoPregunta extends AppCompatActivity{
         botonResp2.setText(respuestasPreg.get(1).getRespuesta());
         botonResp3.setText(respuestasPreg.get(2).getRespuesta());
         botonResp4.setText(respuestasPreg.get(3).getRespuesta());
+
+        //parte de la progressbar
+        progressBar = findViewById(R.id.progressBar);
+
+        handler = new Handler();
+        runnable = new Runnable(){
+            @Override
+            public void run(){
+                if(progress < 30){
+                    progress++;
+                    progressBar.setProgress(progress);
+                    if(progress % 10 == 0){
+                        progressBar.getProgressDrawable().setColorFilter(colors[progress / 10 -1], PorterDuff.Mode.SRC_IN);
+                    }
+                    handler.postDelayed(this, 1000);
+                }
+                else {
+                    //ensenyar popup
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RetoPregunta.this);
+                    View popupView = getLayoutInflater().inflate(R.layout.time_over, null);
+                    builder.setView(popupView);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                    //cerrar el popup con boton
+                    Button buttonClose = popupView.findViewById(R.id.buttonClose);
+                    buttonClose.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v){
+                            dialog.dismiss();
+                        }
+                    });
+                }
+            }
+        };
     }
 
     public List<Answer> recuperarRespuestas (int id_pregunta){
@@ -76,6 +123,7 @@ public class RetoPregunta extends AppCompatActivity{
         }
         return resp;
     }
+
 
 
     public void botonPausar(){;}

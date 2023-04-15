26b6e8c676f27d3822764b80d20cb6148ee4f88c
vic0rv <victor30072002@hotmail.com>
2023-04-15 13:49:55 +0200

    Inicio de sesion casi acabado (si no se borra otra vez)

--- app/src/main/java/com/example/odswix/IniciarSesion.java
+++ app/src/main/java/com/example/odswix/IniciarSesion.java
@@ -9,11 +9,15 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import BusinessLogic.User;
+
 public class IniciarSesion extends AppCompatActivity {
 
+    User usuario = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,63 +47,149 @@ public class IniciarSesion extends AppCompatActivity {
     }
 
     public boolean usernameTest() {
-        EditText textUser = findViewById(R.id.usuario);
+        EditText textUser = findViewById(R.id.txt_username);
         EditText textPass = findViewById(R.id.txt_password);
+        TextView msgError = findViewById(R.id.txt_error);
         String bduser = textUser.getText().toString();
         String pass = textPass.getText().toString();
-        List<String> sql = new ArrayList<>();
-        String password;
+        List<String> sqlUsernames = new ArrayList<>();
+        List<String> sqlPasswords = new ArrayList<>();
+        int i;
         boolean correcto = false;
 
         try {
-            ResultSet rsUser = SingletonSQL.consultar("SELECT username FROM user");
+            //Hacer consulta SQL
+            ResultSet rsUsuario = SingletonSQL.consultar("SELECT username FROM user");
 
-            while (rsUser.next()) {
-                sql.add(rsUser.getString("username"));
+            //Introducir datos de la DB en un array
+            while (rsUsuario.next()) {
+                sqlUsernames.add(rsUsuario.getString("username"));
+            }
+
+            ResultSet rsPass = SingletonSQL.consultar("SELECT password FROM user");
+
+            while (rsPass.next()) {
+                sqlPasswords.add(rsPass.getString("password"));
             }
 
             boolean existe = false;
-            for (int i = 0; i < sql.size(); i++) {
-                if (bduser.equals(sql.get(i))) {
+            for (i = 0; i < sqlUsernames.size(); i++) {
+                if (bduser.equals(sqlUsernames.get(i))) {
                     existe = true;
                     break;
                 }
             }
 
-            TextView msgError = findViewById(R.id.Euser);
+
             if (!existe) {
                 msgError.setText("No existe el usuario introducido");
                 msgError.setVisibility(View.VISIBLE);
             } else {
-                //correcto = true;
-                msgError.setVisibility(View.INVISIBLE);
-                ResultSet rsPass = SingletonSQL.consultar("SELECT password FROM user WHERE username == " + bduser);
 
-                while (rsPass.next()) {
-                    password = rsPass.getString("password");
-                    if (pass == password) {
-                        correcto = true;
-                    } else {
+                msgError.setVisibility(View.INVISIBLE);
+
+
+                if(pass.equals(sqlUsernames.get(i))){
+                    correcto = true;
+                } else {
                         msgError.setText("Credenciales introducidas incorrectas");
                         msgError.setVisibility(View.VISIBLE);
-                    }
-                }
+                        }
             }
 
-        } catch (java.sql.SQLException e) {
-            e.printStackTrace();
-        }
+
+            } catch (java.sql.SQLException e) {
+                e.printStackTrace();
+            }
         return correcto;
     }
 
 
     //Método para iniciar sesión (lo que se ejecuta cuando le das al boton iniciar sesion)
     public void iniciarSesion(View view) {
-        if (isEmptyTest() && usernameTest()) {
-            Intent jugarPartida = new Intent(this, JugarPartida.class);
-            startActivity(jugarPartida);
+
+        try {
+            EditText textUser = findViewById(R.id.txt_username);
+            String bduser = textUser.getText().toString();
+
+            List<Integer> sqlId = new ArrayList<>();
+            List<String> sqlUsernames = new ArrayList<>();
+            List<String> sqlEmails = new ArrayList<>();
+            List<String> sqlPasswords = new ArrayList<>();
+            List<Integer> sqlPuntos = new ArrayList<>();
+            List<Integer> sqlTrofeos = new ArrayList<>();
+            List<Integer> sqlMedallas = new ArrayList<>();
+            int i;
+
+            //Lista usernames
+            ResultSet rsUsernames = SingletonSQL.consultar("SELECT username FROM user");
+            while (rsUsernames.next()) {
+                sqlUsernames.add(rsUsernames.getString("username"));
+            }
+
+            //Lista id
+            ResultSet rsId = SingletonSQL.consultar("SELECT id_user FROM user");
+            while (rsId.next()) {
+                sqlId.add(rsId.getInt("id_user"));
+            }
+
+            //Lista email
+            ResultSet rsEmail = SingletonSQL.consultar("SELECT email FROM user");
+            while (rsEmail.next()) {
+                sqlEmails.add(rsEmail.getString("email"));
+            }
+
+            //Lista passwords
+            ResultSet rsPassword = SingletonSQL.consultar("SELECT password FROM user");
+            while (rsPassword.next()) {
+                sqlPasswords.add(rsPassword.getString("password"));
+            }
+
+            //Lista puntos acumulados totales
+            ResultSet rsPuntos = SingletonSQL.consultar("SELECT puntosAcumTotales FROM user");
+            while (rsPuntos.next()) {
+                sqlPuntos.add(rsPuntos.getInt("puntosAcumTotales"));
+            }
+
+            //Lista trofeos
+            ResultSet rsTrofeos = SingletonSQL.consultar("SELECT trofeos FROM user");
+            while (rsTrofeos.next()) {
+                sqlTrofeos.add(rsTrofeos.getInt("trofeos"));
+            }
+
+            //Lista medallas
+            ResultSet rsMedallas = SingletonSQL.consultar("SELECT medallas FROM user");
+            while (rsMedallas.next()) {
+                sqlMedallas.add(rsMedallas.getInt("medallas"));
+            }
+
+            if (isEmptyTest() && usernameTest()) {
+
+                    for (i = 0; i < sqlUsernames.size(); i++) {
+                        if (bduser.equals(sqlUsernames.get(i))) {
+                            break;
+                        }
+                    }
+
+                    int id = sqlId.get(i);
+                    String username = sqlUsernames.get(i);
+                    String password = sqlPasswords.get(i);
+                    String email = sqlEmails.get(i);
+                    int puntos = sqlPuntos.get(i);
+                    int trofeos = sqlTrofeos.get(i);
+                    int medallas = sqlMedallas.get(i);
+
+                        usuario = new User(id,username,password,email,puntos,trofeos,medallas);
+
+                        Intent jugarPartida = new Intent(this, JugarPartida.class);
+                        jugarPartida.putExtra("user", usuario);
+                        startActivity(jugarPartida);
+                    }
+
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
         }
-    }
 
     //Método para ir a la pantalla de registrarse
     public void registrarse(View view) {

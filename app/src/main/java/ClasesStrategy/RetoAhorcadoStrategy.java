4af8c8d0d290b808666bee128fc0f4401c78bfb5
miguelheru <miguelangelheru@gmail.com>
2023-05-24 14:56:33 +0200

    Strategy Ahorcado

--- app/src/main/java/ClasesStrategy/RetoAhorcadoStrategy.java
+++ app/src/main/java/ClasesStrategy/RetoAhorcadoStrategy.java
@@ -1,6 +1,9 @@
 package ClasesStrategy;
 
 import android.content.Context;
+import android.content.Intent;
+
+import com.example.odswix.Gestor;
 
 import BusinessLogic.User;
 
@@ -8,6 +11,15 @@ public class RetoAhorcadoStrategy implements Strategy {
 
     @Override
     public void aplicarTipo(Context context, String tipo, User usuario, boolean appMuted) {
-
+        Intent intent = new Intent(context, Gestor.class);
+        intent.putExtra("tipo",tipo);
+        intent.putExtra("user", usuario);
+        intent.putExtra("muted", appMuted);
+        intent.putExtra("numPreg", 1);
+        intent.putExtra("puntosAcum", 0);
+        intent.putExtra("puntosCons", 0);
+        intent.putExtra("vidasPreg", 2);
+        intent.putExtra("consolidado", false);
+        context.startActivity(intent);
     }
 }

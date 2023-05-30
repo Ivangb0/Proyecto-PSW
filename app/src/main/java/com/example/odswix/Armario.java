d93c8de03effb7cdd5669f7951461edb119d7096
vic0rv <victor30072002@hotmail.com>
2023-05-30 20:05:14 +0200

    arreglos en diseño, ahora luego sigo

--- app/src/main/java/com/example/odswix/Armario.java
+++ app/src/main/java/com/example/odswix/Armario.java
@@ -13,6 +13,8 @@ import BusinessLogic.User;
 
 public class Armario extends AppCompatActivity {
     User usuario = null;
+    TextView tv_medallas;
+    Boolean appMuted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,34 +26,24 @@ public class Armario extends AppCompatActivity {
 
         setContentView(R.layout.armario);
 
+        tv_medallas = findViewById(R.id.medallas);
+
         Intent intent = getIntent();
         usuario = (User) intent.getSerializableExtra("user");
-
-        setData(usuario);
+        appMuted = intent.getBooleanExtra("muted", false);
+        setData();
     }
 
-    private void setData(User user){
-        if(usuario.ganadas < 5){ user.setMedallas(0); }
-        else if(usuario.ganadas < 10 && usuario.ganadas > 0){ user.setMedallas(1); }
-        else if(usuario.ganadas < 15 && usuario.ganadas > 0){ user.setMedallas(2); }
-        else if(usuario.ganadas < 20 && usuario.ganadas > 0){ user.setMedallas(3); }
-        else if(usuario.ganadas < 25 && usuario.ganadas > 0){ user.setMedallas(4); }
-        else if(usuario.ganadas < 30 && usuario.ganadas > 0){ user.setMedallas(5); }
-        else if(usuario.ganadas < 35 && usuario.ganadas > 0){ user.setMedallas(6); }
-        else if(usuario.ganadas < 40 && usuario.ganadas > 0){ user.setMedallas(7); }
-        else if(usuario.ganadas < 45 && usuario.ganadas > 0){ user.setMedallas(8); }
-        else if(usuario.ganadas < 50 && usuario.ganadas > 0){ user.setMedallas(9); }
-        else if(usuario.ganadas < 55 && usuario.ganadas > 0){ user.setMedallas(10); }
-        else if(usuario.ganadas < 60 && usuario.ganadas > 0){ user.setMedallas(11); }
-        else if(usuario.ganadas < 65 && usuario.ganadas > 0){ user.setMedallas(12); }
-
-        TextView medallas = findViewById(R.id.medallas);
-        medallas.setText(String.valueOf(user.getMedallas()));
+    private void setData(){
+        int ganadas = usuario.ganadas;
+        int medallas = ganadas / 5;
+        tv_medallas.setText(String.valueOf(medallas));
     }
 
     public void volver (View view){
         Intent perfil = new Intent(this, Perfil.class);
         perfil.putExtra("user", usuario);
+        perfil.putExtra("muted", appMuted);
         finishAfterTransition();
         startActivity(perfil);
     }
